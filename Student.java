aa00764bc2f852576f2a9af6c7c3f852d385fc9e
NightyCoder <dimitar.demirovski@gmail.com>
2020-03-28 18:36:57 +0100

    student class finished

--- Student.java
+++ Student.java
@@ -1,18 +1,63 @@
 class Student {
-	String index;
-	String firstName;
-	String lastName;
-	int grades[]
+    String index;
+    String firstName;
+    String lastName;
+    int[] grades;
 
-	//TODO constructor
+    //TODO constructor
+    public Student(String index, String firstName, String lastName, int[] grades) {
+        this.index = index;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.grades = grades;
+    }
 
-	//TODO seters & getters
+    //TODO seters & getters
+    public String getIndex() {
+        return index;
+    }
 
-	public double getAverage() {
-		//TODO
-	}
+    public void setIndex(String index) {
+        this.index = index;
+    }
 
-	public int ECTSCredits() {
-		//TODO
-	}
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int[] getGrades() {
+        return grades;
+    }
+
+    public void setGrades(int[] grades) {
+        this.grades = grades;
+    }
+
+    public double getAverage() {
+        //TODO
+        double sum = 0;
+        for (int grade : grades) sum += grade;
+        return sum / grades.length;
+    }
+
+    public int ECTSCredits() {
+        //TODO
+        int credits = 0;
+        for (int grade : grades) {
+            if (grade >= 6) credits += 6;
+        }
+        return credits;
+    }
 }
